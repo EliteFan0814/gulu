fea37da24ca99ccb31f588e5fbbec16a0507ce48
EliteFan <fan552426811@outlook.com>
2019-08-19 23:27:12 +0800

    完成input

--- src/input.vue
+++ src/input.vue
@@ -2,8 +2,8 @@
     <div class="wrapper">
         <input :value="value" :disabled="disabled" :readonly="readonly" :class="{error}" type="text">
         <template v-if="error">
-            <g-icon name="error"></g-icon>
-            <span>{{error}}</span>
+            <g-icon name="error" class="icon-error"></g-icon>
+            <span class="icon-error">{{error}}</span>
         </template>
     </div>
 </template>
@@ -41,28 +41,15 @@
     $box-shadow-color: rgba(0,0,0,0.5);
     $red: #f1453d;
     .wrapper{
-        font-size: $font-size;
-        display: inline-block;
+        font-size: $font-size;display: inline-flex;align-items: center;
+        > :not(:last-child){margin-right: .5em;}
         > input{
-            height: 32px;
-            border: 1px solid $border-color;
-            border-radius: 4px;
-            padding: 0 8px;
-            font-size: inherit;
-            &:hover{
-                border-color: $border-color-hover;
-            }
-            &:focus{
-                box-shadow: inset 1 1px 3px $box-shadow-color;
-            }
-            &[disabled],&[readonly]{
-                border-color: #ccc;
-                color: #ccc;
-                cursor: not-allowed;
-            }
-            &.error{
-                border-color: $red;
-            }
+            height: 32px;border: 1px solid $border-color;border-radius: 4px;padding: 0 8px;font-size: inherit;
+            &:hover{border-color: $border-color-hover;}
+            &:focus{box-shadow: inset 1 1px 3px $box-shadow-color;}
+            &[disabled],&[readonly]{border-color: #ccc;color: #ccc;cursor: not-allowed;}
+            &.error{border-color: $red;}
         }
+        .icon-error{fill: $red;color: $red;}
     }
 </style>
