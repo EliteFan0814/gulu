86c70748d8b531732c38fe17e07b73bc28ada9e2
EliteFan <fan552426811@outlook.com>
2019-09-25 21:21:27 +0800

    添加简单动画

--- src/app.js
+++ src/app.js
@@ -7,6 +7,11 @@ import chai from 'chai'
 import spies from 'chai-spies'
 import Row from './row'
 import Col from './col'
+import Layout from './layout'
+import Header from './header'
+import Sider from './sider'
+import Footer from './footer'
+import Content from './content'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -14,6 +19,12 @@ Vue.component('g-button-group', ButtonGroup)
 Vue.component('g-input',Input)
 Vue.component('g-row',Row)
 Vue.component('g-col',Col)
+Vue.component('g-layout',Layout)
+Vue.component('g-header',Header)
+Vue.component('g-sider',Sider)
+Vue.component('g-footer',Footer)
+Vue.component('g-content',Content)
+
 chai.use(spies)
 
 new Vue({
